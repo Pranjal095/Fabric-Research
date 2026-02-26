0d93cff5bf484e9214eefd204ace6d9696f75b42
Pranjal095 <pranjal.prajapati35@gmail.com>
2026-02-27 02:13:05 +0530

    fix: enforce configtxgen path

--- deploy/generate_crypto.sh
+++ deploy/generate_crypto.sh
@@ -12,8 +12,8 @@ rm -rf crypto-config mychannel.block
 echo "Generating crypto materials for 1 Orderer and 7 Peers..."
 ../build/bin/cryptogen generate --config=./crypto-config.yaml --output="crypto-config"
 
-echo "Generating Orderer genesis and channel block (mychannel.block)..."
-export FABRIC_CFG_PATH=$PWD
-../build/bin/configtxgen -profile TwoOrgsApplicationGenesis -channelID mychannel -outputBlock ./mychannel.block
+echo "Generating Orderer genesis and channel block (mychannel.block) exclusively from ${PWD}/configtx.yaml..."
+export FABRIC_CFG_PATH=${PWD}
+../build/bin/configtxgen -profile TwoOrgsApplicationGenesis -channelID mychannel -outputBlock ./mychannel.block -configPath ${PWD}
 
 echo "Done! Crypto materials are in deploy/crypto-config/ and mychannel.block is ready."
