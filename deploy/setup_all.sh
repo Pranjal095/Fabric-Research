739b331d9fd99d104437e470f59cdddb0b27a6d0
Pranjal095 <pranjal.prajapati35@gmail.com>
2026-03-01 13:27:54 +0530

    fix: fix deployment script

--- deploy/setup_all.sh
+++ deploy/setup_all.sh
@@ -7,7 +7,7 @@ START_INDEX=${1:-0}
 
 export FABRIC_CFG_PATH=$PWD/../sampleconfig
 export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_TLS_ROOTCERT_FILE=$PWD/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_TLS_ROOTCERT_FILE=$PWD/crypto-config/peerOrganizations/org1.example.com/peers/peer${START_INDEX}.org1.example.com/tls/ca.crt
 export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_MSPCONFIGPATH=$PWD/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 
